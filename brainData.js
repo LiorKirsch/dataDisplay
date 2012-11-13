5c06628ec7fd092ab7f2de3e6404fd91d6fd4d3f
Lior Kirsch <lior@lior.(none)>
2012-11-13 13:53:18 +0200

    data

---
+++ brainData.js
@@ -0,0 +1,209 @@
+var treeData = {"name":"Basic cell groups and regions","children" :[
+{"name":"Cerebrum","children" :[
+{"name":"Cerebral cortex","children" :[
+{"name":"Cortical plate","children" :[
+{"name":"Olfactory areas","children" :[
+{"name":"Main olfactory bulb"}
+,{"name":"Accessory olfactory bulb"}
+,{"name":"Anterior olfactory nucleus"}
+,{"name":"Taenia tecta"}
+,{"name":"Piriform area"}
+,{"name":"Nucleus of the lateral olfactory tract"}
+,{"name":"Piriform-amygdalar area"}
+,{"name":"Cortical amygdalar area"}
+,{"name":"Postpiriform transition area"}]}
+,{"name":"Hippocampal formation","children" :[
+{"name":"Hippocampal region","children" :[
+{"name":"Ammon's Horn"}
+,{"name":"Dentate gyrus"}]}
+,{"name":"Retrohippocampal region","children" :[
+{"name":"Subiculum"}]}]}]}]}
+,{"name":"Cerebral nuclei","children" :[
+{"name":"Striatum","children" :[
+{"name":"Striatum dorsal region","children" :[
+{"name":"Caudoputamen "}]}
+,{"name":"Striatum ventral region","children" :[
+{"name":"Nucleus accumbens "}
+,{"name":"Fundus of striatum"}
+,{"name":"Olfactory tubercle "}]}
+,{"name":"Lateral septal complex","children" :[
+{"name":"Lateral septal nucleus "}
+,{"name":"Septofimbrial nucleus "}]}
+,{"name":"Striatum-like amygdalar nuclei","children" :[
+{"name":"Central amygdalar nucleus "}
+,{"name":"Anterior amygdalar area"}
+,{"name":"Medial amygdalar nucleus "}]}]}
+,{"name":"Pallidum","children" :[
+{"name":"Pallidum_ dorsal region"}
+,{"name":"Pallidum_ ventral region","children" :[
+{"name":"Magnocellular nucleus"}
+,{"name":"Substantia innominata"}]}
+,{"name":"Pallidum_ caudal region","children" :[
+{"name":"Bed nucleus of the anterior commissure "}
+,{"name":"Bed nuclei of the stria terminalis "}]}
+,{"name":"Pallidum_ medial region"}]}]}]}
+,{"name":"Brain stem","children" :[
+{"name":"Interbrain","children" :[
+{"name":"Thalamus","children" :[
+{"name":"Thalamus_ polymodal association cortex related","children" :[
+{"name":"Anterior group of the dorsal thalamus","children" :[
+{"name":"Anterodorsal nucleus"}
+,{"name":"Anteromedial nucleus"}
+,{"name":"Anteroventral nucleus of thalamus "}
+,{"name":"Interanterodorsal nucleus of the thalamus"}
+,{"name":"Interanteromedial nucleus of the thalamus"}
+,{"name":"Lateral dorsal nucleus of thalamus"}]}
+,{"name":"Reticular nucleus of the thalamus"}
+,{"name":"Lateral group of the dorsal thalamus","children" :[
+{"name":"Lateral posterior nucleus of the thalamus"}
+,{"name":"Suprageniculate nucleus"}]}
+,{"name":"Geniculate group_ ventral thalamus","children" :[
+{"name":"Intergeniculate leaflet of the lateral geniculate complex"}
+,{"name":"Ventral part of the lateral geniculate complex"}]}
+,{"name":"Intralaminar nuclei of the dorsal thalamus","children" :[
+{"name":"Central lateral nucleus of the thalamus"}
+,{"name":"Central medial nucleus of the thalamus"}
+,{"name":"Paracentral nucleus"}
+,{"name":"Parafascicular nucleus"}]}
+,{"name":"Epithalamus","children" :[
+{"name":"Lateral habenula"}
+,{"name":"Medial habenula"}]}
+,{"name":"Medial group of the dorsal thalamus","children" :[
+{"name":"Intermediodorsal nucleus of the thalamus"}
+,{"name":"Mediodorsal nucleus of thalamus"}
+,{"name":"Perireunensis nucleus"}]}]}
+,{"name":"Thalamus_ sensory-motor cortex related","children" :[
+{"name":"Geniculate group_ dorsal thalamus","children" :[
+{"name":"Dorsal part of the lateral geniculate complex"}
+,{"name":"Medial geniculate complex"}]}
+,{"name":"Peripeduncular nucleus"}
+,{"name":"Ventral group of the dorsal thalamus","children" :[
+{"name":"Ventral medial nucleus of the thalamus"}
+,{"name":"Ventral posterior complex of the thalamus"}]}]}]}
+,{"name":"Hypothalamus","children" :[
+{"name":"Periventricular zone","children" :[
+{"name":"Arcuate hypothalamic nucleus"}
+,{"name":"Paraventricular hypothalamic nucleus"}]}
+,{"name":"Periventricular region","children" :[
+{"name":"Anterodorsal preoptic nucleus"}
+,{"name":"Anteroventral preoptic nucleus "}
+,{"name":"Anteroventral periventricular nucleus"}
+,{"name":"Dorsomedial nucleus of the hypothalamus"}
+,{"name":"Median preoptic nucleus"}
+,{"name":"Posterodorsal preoptic nucleus"}
+,{"name":"Parastrial nucleus"}
+,{"name":"Suprachiasmatic nucleus"}]}
+,{"name":"Hypothalamic medial zone","children" :[
+{"name":"Anterior hypothalamic nucleus"}
+,{"name":"Mammillary body","children" :[
+{"name":"Lateral mammillary nucleus"}
+,{"name":"Medial mammillary nucleus"}
+,{"name":"Supramammillary nucleus"}]}
+,{"name":"Medial preoptic nucleus"}
+,{"name":"Posterior hypothalamic nucleus"}
+,{"name":"Dorsal premammillary nucleus"}
+,{"name":"Ventral premammillary nucleus"}
+,{"name":"Ventromedial hypothalamic nucleus"}]}
+,{"name":"Hypothalamic lateral zone","children" :[
+{"name":"Subthalamic nucleus"}
+,{"name":"Tuberal nucleus"}
+,{"name":"Zona incerta"}]}]}]}
+,{"name":"Midbrain","children" :[
+{"name":"Midbrain_ sensory related","children" :[
+{"name":"Inferior colliculus"}
+,{"name":"Midbrain trigeminal nucleus"}
+,{"name":"Nucleus of the brachium of the inferior colliculus"}
+,{"name":"Parabigeminal nucleus"}
+,{"name":"Nucleus sagulum"}
+,{"name":"Superior colliculus_ sensory related"}]}
+,{"name":"Midbrain_ motor related","children" :[
+{"name":"Superior colliculus_ motor related"}
+,{"name":"Substantia nigra_ reticular part"}
+,{"name":"Ventral tegmental area"}
+,{"name":"Midbrain reticular nucleus_ retrorubral area"}
+,{"name":"Periaqueductal gray","children" :[
+{"name":"Interstitial nucleus of Cajal"}]}
+,{"name":"Pretectal region","children" :[
+{"name":"Anterior pretectal nucleus "}
+,{"name":"Medial pretectal area"}
+,{"name":"Nucleus of the optic tract"}
+,{"name":"Nucleus of the posterior commissure"}
+,{"name":"Olivary pretectal nucleus"}]}
+,{"name":"Midbrain reticular nucleus_ magnocellular part_ general","children" :[
+{"name":"Ventral tegmental nucleus"}
+,{"name":"Anterior tegmental nucleus "}]}
+,{"name":"Cuneiform nucleus"}
+,{"name":"Red Nucleus"}
+,{"name":"Oculomotor nucleus"}
+,{"name":"Edinger-Westphal nucleus"}
+,{"name":"Trochlear nucleus"}]}
+,{"name":"Midbrain_ behavioral state related","children" :[
+{"name":"Substantia nigra_ compact part"}
+,{"name":"Pedunculopontine nucleus"}
+,{"name":"Midbrain raph� nuclei","children" :[
+{"name":"Central linear nucleus raph�"}
+,{"name":"Dorsal nucleus raph�"}
+,{"name":"Interpeduncular nucleus"}]}]}]}
+,{"name":"Hindbrain","children" :[
+{"name":"Pons","children" :[
+{"name":"Pons_ sensory related","children" :[
+{"name":"Nucleus of the lateral lemniscus"}
+,{"name":"Parabrachial nucleus"}
+,{"name":"Principal sensory nucleus of the trigeminal"}
+,{"name":"Superior olivary complex"}]}
+,{"name":"Pons_ motor related","children" :[
+{"name":"Barrington's nucleus "}
+,{"name":"Dorsal tegmental nucleus"}
+,{"name":"Pontine central gray"}
+,{"name":"Pontine gray"}
+,{"name":"Supragenual nucleus"}
+,{"name":"Supratrigeminal nucleus"}
+,{"name":"Tegmental reticular nucleus"}
+,{"name":"Motor nucleus of trigeminal"}]}
+,{"name":"Pons_ behavioral state related","children" :[
+{"name":"Superior central nucleus raph�"}
+,{"name":"Locus ceruleus"}
+,{"name":"Nucleus incertus"}
+,{"name":"Nucleus raph� pontis"}
+,{"name":"Subceruleus nucleus"}
+,{"name":"Sublaterodorsal nucleus"}]}]}
+,{"name":"Medulla","children" :[
+{"name":"Medulla_ sensory related","children" :[
+{"name":"Area postrema"}
+,{"name":"Cochlear nuclei "}
+,{"name":"Dorsal column nuclei","children" :[
+{"name":"Cuneate nucleus"}
+,{"name":"Gracile nucleus"}]}
+,{"name":"External cuneate nucleus"}
+,{"name":"Nucleus of the solitary tract"}
+,{"name":"Spinal nucleus of the trigeminal_ caudal part"}
+,{"name":"Spinal nucleus of the trigeminal_ interpolar part"}
+,{"name":"Spinal nucleus of the trigeminal_ oral part"}]}
+,{"name":"Medulla_ motor related","children" :[
+{"name":"Abducens nucleus"}
+,{"name":"Facial motor nucleus"}
+,{"name":"Nucleus ambiguus"}
+,{"name":"Dorsal motor nucleus of the vagus nerve"}
+,{"name":"Inferior olivary complex"}
+,{"name":"Inferior salivatory nucleus"}
+,{"name":"Linear nucleus of the medulla"}
+,{"name":"Lateral reticular nucleus"}
+,{"name":"Magnocellular reticular nucleus"}
+,{"name":"Parasolitary nucleus"}
+,{"name":"Paragigantocellular reticular nucleus"}
+,{"name":"Parapyramidal nucleus"}
+,{"name":"Vestibular nuclei","children" :[
+{"name":"Lateral vestibular nucleus"}
+,{"name":"Medial vestibular nucleus"}]}
+,{"name":"Hypoglossal nucleus"}
+,{"name":"Nucleus x"}
+,{"name":"Nucleus y"}]}
+,{"name":"Medulla_ behavioral state related","children" :[
+{"name":"Nucleus raph� magnus"}
+,{"name":"Nucleus raph� obscurus"}]}]}]}]}
+,{"name":"Cerebellum","children" :[
+{"name":"Cerebellar nuclei","children" :[
+{"name":"Dentate nucleus"}
+,{"name":"Fastigial nucleus"}
+,{"name":"Interposed nucleus"}]}
+,{"name":"Cerebellar cortex"}]}]}
